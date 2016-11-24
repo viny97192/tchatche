446670148cf232176b5c3a76aa7e50f7c488d9d9
viny971 <viny971@gmail.com>
2016-11-24 19:11:27 +0100

    réécriture de tchatche_serveur avec des listes chaînées

--- tchatche_serveur.c
+++ tchatche_serveur.c
@@ -7,35 +7,31 @@
 
 int main(int argc, char *argv[]){
 	
-	user *u = init_user(1,"amine","toto"), *u2 = init_user(2,"vincent","tutu");
-	utilisateurs *ut = init_utilisateurs();
+	user *u = init_user(0,"amine","toto"), *u2 = init_user(1,"vincent","tutu"), *u3 = init_user(2,"andy","tata"), *u4 = init_user(3,"paul","titi"), *u5 = init_user(4,"mathieu","tete"), *u6 = init_user(5,"paul","tyty");
+	utilisateurs *ut = init_utilisateurs(u);
+	int n;
+	
+	add_user(ut -> l,u2);
+	add_user(ut -> l,u3);
+	add_user(ut -> l,u4);
+	add_user(ut -> l,u5);
+	add_user(ut -> l,u6);
+	print_utilisateurs(ut -> l);
+	n = nb_users(ut -> l);
+	printf("n = %d\n\n\n", n);
 
-	add_user(ut,u);
-	print_user(&(ut -> t[0]));
-	add_user(ut,u2);
-	print_user(&(ut -> t[1]));
+	ut -> l = delete_user(ut -> l, u5);
+	print_utilisateurs(ut -> l);
 
-	/*
-	int fd;
-	char *fifo = "tchatche_serveur";
-	user connected[1];
-
-	if((mkfifo(fifo, S_IRWXU | S_IROTH | S_IWOTH | S_IRGRP)) == -1){
-		perror("mkfifo ");
-		exit(EXIT_FAILURE);
-	}
-
-	if((fd = open(fifo, O_RDWR)) == -1){
-		perror("open ");
-		exit(EXIT_FAILURE);
-	}
-
-	while(type != 8){
-			
-	}	
-	*/
-
-
+	free_utilisateurs(ut);
+	free_user(u);
+	free_user(u2);
+	free_user(u3);
+	free_user(u4);
+	free_user(u5);
+	free_user(u6);
+	
+	free_utilisateurs(ut);
 
 	return 0;
 }
@@ -65,6 +61,8 @@ int type_message(char *message){
 		t = 9;
 	if(!strcmp(s,"FILE"))
 		t = 10;
+	if(!strcmp(s,"SHUT"))
+		t = 11;
 
 	return t;
 }
@@ -96,50 +94,191 @@ user *init_user(int id, char *pseudo, char *pipe){
 	return u;
 }
 
-void print_user(user *u){
-	fprintf(stdout,"id \t\t: %d\npseudo \t\t: %s\ntube \t\t: %s\n\n", u -> id, u -> pseudo, u -> pipe);
+user_list *init_user_list(user *usr){
+	user_list *l = (user_list *)malloc(sizeof(*l));
+
+	l -> val = *usr;
+	l -> next = NULL;
+
+	return l;
 }
 
-utilisateurs *init_utilisateurs(){
+utilisateurs *init_utilisateurs(user *usr){
 	
 	utilisateurs *ut = (utilisateurs *)malloc(sizeof(*ut));
 
-	ut -> t = (user *)malloc(sizeof(*(ut -> t)));
-	ut -> t[0].id = -1;
-	ut -> capacite = 1;
-	ut -> occupation = 0;
+	ut -> l = init_user_list(usr);
+	ut -> nb_users = 0;
+	ut -> l -> next = NULL;
 
 	return ut;
 }
 
-/*
-void print_utilisateurs(utilisateurs *ut){
-	fprintf(stdout,"")
+void free_user(user *u){
+	free(u);
 }
-*/
 
-void add_user(utilisateurs *ut, user *usr){
-	int i;
+void free_utilisateurs(utilisateurs *ut){
+	
+	user_list *tmp;
+
+	if(ut == NULL || ut -> l == NULL)
+		return;
+	
+	if(ut -> l -> next == NULL){
+		free(ut -> l);
+		free(ut);
+		return;
+	}
+	
+	while(ut -> l != NULL){
+		tmp = ut -> l -> next;
+		free(ut -> l);
+		ut -> l = tmp;
+	}
+
+	free(ut);
+}
+
+void print_user(user *u){
+	fprintf(stdout,"id \t\t: %d\npseudo \t\t: %s\ntube \t\t: %s\n\n", u -> id, u -> pseudo, u -> pipe);
+}
+
+void print_utilisateurs(user_list *l){
+	if(l == NULL){
+		fprintf(stderr,"Attention, aucun utilisateur n'est connecté.\n");
+		return;
+	}
+
+	if(l -> next == NULL){
+		print_user(&(l -> val));
+		return;
+	}
+
+	print_user(&(l -> val));
+	print_utilisateurs(l -> next);
+}
+
+int add_user(user_list *l, user *usr){
+	int taken;
+
+	if(l == NULL){
+		fprintf(stderr,"Erreur, le serveur n'a pas été initalisé.\n");
+		return -2;
+	}
+
 	if(usr == NULL){
-		fprintf(stderr,"Erreur, utilisateur non valide.\n");
-		return;
+		fprintf(stderr,"Erreur, l'utilisateur n'a pas été initalisé.\n");
+		return -1;
 	}
 
-	if(ut -> capacite == ut -> occupation){
-		ut -> t = realloc(ut -> t, (2*(ut -> capacite))*sizeof(user));
-		ut -> capacite *= 2;
-		ut -> t[ut -> occupation] = *usr;
-		usr -> id = ut -> occupation;
-		ut -> occupation += 1;
-		return;
+	if((taken = taken_login(l, usr))){
+		return taken;
 	}
-	
 
-	for(i=0;i<ut -> capacite;i++){
-		if((ut -> t[i].id) == -1){
-			ut -> t[i] = *usr;
-			ut -> occupation += 1;
+	if(l -> next == NULL){
+		user_list *lusr = init_user_list(usr);
+		l -> next = lusr;
+		return 4;
+	}
+
+	return add_user(l -> next,usr);
+}
+
+user_list *delete_user(user_list *l, user *usr){
+
+	user_list *tmp = l, *tmp2;
+
+	if(l == NULL){
+		fprintf(stderr,"Erreur, aucun utilisateur n'est connecté.\n");
+		return NULL;
+	}
+
+	if(usr == NULL){
+		fprintf(stderr,"Erreur, l'utilisateur n'a pas été initalisé.\n");
+		return l;
+	}
+
+	if(!taken_login(l,usr)){
+		fprintf(stderr,"Attention, cet utilisateur n'est pas connecté.\n");
+		return l;
+	}
+
+	if(l -> next == NULL){
+		free(l);	
+		return NULL;
+	}
+
+	if(!strcmp(l -> val.pseudo,usr -> pseudo)){
+		l = l -> next;
+		free(tmp);
+		return l;
+	}
+
+	if(!strcmp(l -> next -> val.pseudo,usr -> pseudo)){
+		tmp = l -> next;
+		l -> next = l -> next -> next;
+		free(tmp);
+		return l;
+	}
+
+	while(tmp -> next -> next != NULL){
+		if(!strcmp(tmp -> next -> val.pseudo,usr -> pseudo)){
+			tmp2 = tmp -> next;
+			tmp -> next = tmp -> next -> next;
+			free(tmp2);
+			return l;
 		}
+
+		tmp = tmp -> next;
 	}
+
+	free(tmp -> next);
+	tmp -> next = NULL;
+	return l;
+}
+
+int nb_users(user_list *l){
+	if(l == NULL)
+		return 0;
+	if(l -> next == NULL)
+		return 1;
+	return 1 + nb_users(l -> next);
+}
+
+int compare(user *usr1, user *usr2){
+	int taken = 0;
+
+	if(usr1 == NULL||usr2 == NULL)
+		taken = -1;
 	
+	else if(!strcmp(usr1 -> pseudo, usr2 -> pseudo) && !strcmp(usr1 -> pipe, usr2 -> pipe))
+		taken = 3;
+	else if(!strcmp(usr1 -> pseudo, usr2 -> pseudo))
+		taken = 1;
+	else if(!strcmp(usr1 -> pipe, usr2 -> pipe))
+		taken = 2;
+
+	return taken;
+}
+
+int taken_login(user_list *l, user *usr){
+	if(l == NULL){
+		fprintf(stderr,"Attention, liste d'utilisateurs non initialisée.\n");
+		return -1;
+	}
+
+	if(usr == NULL){
+		fprintf(stderr,"Attention, utilisateur non initialisé.\n");
+		return -1;
+	}
+
+	if(l -> next == NULL){
+		return compare(&(l -> val), usr);	
+	}
+
+	if(!compare(&(l -> val),usr)){
+		return taken_login(l -> next,usr);
+	}
+	return compare(&(l -> val),usr);
 }
--- tchatche_serveur.h
+++ tchatche_serveur.h
@@ -1,13 +1,22 @@
+typedef struct user_list user_list;
+
 typedef struct user{
 	int id;
 	char *pseudo;
 	char *pipe;
 }user;
 
+struct user_list{
+	user val;
+	user_list *next;
+};
+
 typedef struct utilisateurs{
-	user *t;
+	user_list *l;
+	/*
 	int capacite;
-	int occupation;	
+	*/
+	int nb_users;	
 }utilisateurs;
 
 /*
@@ -20,7 +29,9 @@ typedef struct message{
 
 user *init_user(int id, char *pseudo, char *pipe);
 
-utilisateurs *init_utilisateurs();
+user_list *init_user_list(user *usr);
+
+utilisateurs *init_utilisateurs(user *usr);
 
 void free_user(user *u);
 
@@ -28,12 +39,18 @@ void free_utilisateurs(utilisateurs *ut);
 
 void print_user(user *u);
 
-void print_utilisateurs(utilisateurs *ut);
+void print_utilisateurs(user_list *l);
 
-void add_user(utilisateurs *ut, user *usr);
+int add_user(user_list *l, user *usr);
 
-void delete_user(user *usr);
+user_list *delete_user(user_list *l, user *usr);
+
+int nb_users(user_list *l);
 
 int type_message(char *message);
 
 char *type(char *message);
+
+int compare(user *usr1, user *usr2);
+
+int taken_login(user_list *l, user *usr);
